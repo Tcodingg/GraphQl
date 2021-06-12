b2afe2f9e1567802666545a35c66a5c8e317e4c0
DePrincio <princetlive@yahoo.com>
2021-06-12 15:56:40 -0400

    setup graphqlSchema

--- server/index.js
+++ server/index.js
@@ -2,5 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const userData = require('./MOCK_DATA.json');
+const graphql = require('graphql');
+const graphqlHTTP = require('express-graphql');
 
+const RootQuery = 'query';
+const mutation = 'mutation';
+
+const schema = new GraphQLSchema({
+	query: RootQuery,
+	mutation: mutation,
+});
+app.use(
+	'/grapql',
+	graphqlHTTP({
+		schema,
+		graphql: true,
+	})
+);
 app.listen(PORT, () => console.log('server is running'));
