9f7c7f0a72020ea15c0191fbfcbc2c47efb02a94
DePrincio <princetlive@yahoo.com>
2021-06-15 14:46:00 -0400

    add userType and rootQuery

--- server/index.js
+++ server/index.js
@@ -2,10 +2,39 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const userData = require('./MOCK_DATA.json');
-const graphql = require('graphql');
+const graphql, {GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList} = require('graphql');
 const graphqlHTTP = require('express-graphql');
 
-const RootQuery = 'query';
+const userType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: {
+      type: GraphQLInt
+    },
+     firstName: {
+      type: GraphQLString
+     },
+      lastName: {
+      type: GraphQLString
+    },
+      email: {
+      type: GraphQLString
+    },
+      password: {
+      type: GraphQLString
+    }
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    getAllUsers: {
+      type: new GraphQLList(UserType), 
+    }
+
+  }
+});
 const mutation = 'mutation';
 
 const schema = new GraphQLSchema({
