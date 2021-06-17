ca348c1722fb8dc816447bf8bbfc5c2b76bb839b
DePrincio <princetlive@yahoo.com>
2021-06-16 21:06:15 -0400

    add mutation

--- server/index.js
+++ server/index.js
@@ -2,40 +2,77 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const userData = require('./MOCK_DATA.json');
-const graphql, {GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList} = require('graphql');
+const graphql = require('graphql');
+const {
+	GraphQLObjectType,
+	GraphQLSchema,
+	GraphQLInt,
+	GraphQLString,
+	GraphQLList,
+} = graphql;
 const graphqlHTTP = require('express-graphql');
 
 const userType = new GraphQLObjectType({
-  name: 'User',
-  fields: () => ({
-    id: {
-      type: GraphQLInt
-    },
-     firstName: {
-      type: GraphQLString
-     },
-      lastName: {
-      type: GraphQLString
-    },
-      email: {
-      type: GraphQLString
-    },
-      password: {
-      type: GraphQLString
-    }
-  })
-})
+	name: 'User',
+	fields: () => ({
+		id: {
+			type: GraphQLInt,
+		},
+		firstName: {
+			type: GraphQLString,
+		},
+		lastName: {
+			type: GraphQLString,
+		},
+		email: {
+			type: GraphQLString,
+		},
+		password: {
+			type: GraphQLString,
+		},
+	}),
+});
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
-  fields: {
-    getAllUsers: {
-      type: new GraphQLList(UserType), 
-    }
-
-  }
+	name: 'RootQueryType',
+	fields: {
+		getAllUsers: {
+			type: new GraphQLList(UserType),
+		},
+	},
 });
-const mutation = 'mutation';
+const Mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	filds: {
+		createUser: {
+			type: userType,
+			args: {
+				firstName: {
+					type: GraphQLString,
+				},
+				lastName: {
+					type: GraphQLString,
+				},
+				email: {
+					type: GraphQLString,
+				},
+				password: {
+					type: GraphQLString,
+				},
+				resolve(parent, args) {
+					userData.push({
+						id: userData.length + 1,
+						firstName: args.firstName,
+						lastName: args.lastName,
+						email: args.email,
+						password: args.password,
+					});
+					return args;
+				},
+			},
+		},
+	},
+});
 
 const schema = new GraphQLSchema({
 	query: RootQuery,
